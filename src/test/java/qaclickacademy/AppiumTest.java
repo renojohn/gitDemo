0ecc717ff5991f2dd98e27162c6e539f0c9a3e29
reno <reno.varghese@gmail.com>
2019-07-02 12:42:18 -0400

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeApp() {
+		System.out.println("NativeApp");
+	}
+
+	@Test
+	public void AndroidApp() {
+		System.out.println("AndroidApp");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumCodeTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumCodeTest {
+
+	@Test
+	public void Browser() {
+		System.out.println("Browser");
+	}
+
+	@Test
+	public void elementUI() {
+		System.out.println("elementUI");
+
+	}
+
+}
